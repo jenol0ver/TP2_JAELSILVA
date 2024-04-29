67c45fa2fa3c314606e6c7979e1635f5b23d6bb4
Intel <Intel@DESKTOP-KVMM6CQ>
2024-04-29 17:03:30 -0300

    ejericio03 terminado:33

--- TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
+++ TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
@@ -1,5 +1,44 @@
 package ar.edu.unju.fi.ejercicio03.constantes;
 
-public class Provincia {
+public enum Provincia {
+	JUJUY(770881, 53219), SALTA(1333365, 155488),
+    TUCUMAN(1687305, 22524), CATAMARCA(367820, 102606),
+    LA_RIOJA(331847, 89680), SANTIAGO_DEL_ESTERO(874006, 136351);
+	
+	private int cantidadPoblacion;
+    private int superficie;
+	
+    private Provincia() {
+		// TODO Auto-generated constructor stub
+	}   
+    
+	private Provincia(int cantidadPoblacion, int superficie) {
+		this.cantidadPoblacion = cantidadPoblacion;
+		this.superficie = superficie;
+	}
+
+	public int getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+
+	public void setCantidadPoblacion(int cantidadPoblacion) {
+		this.cantidadPoblacion = cantidadPoblacion;
+	}
+
+	public int getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
+
+	public Double densidadPoblacional() {
+        return (double) cantidadPoblacion / superficie;
+    }	
+	
+	
+	
+	
 
 }
