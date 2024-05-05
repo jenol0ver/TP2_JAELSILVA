c8b1edd8ebd5c8a75f4100ee5e266eb838f96ac5
Intel <Intel@DESKTOP-KVMM6CQ>
2024-05-05 17:45:54 -0300

    EJERCICIO 5 TERMINADO

--- TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio05/model/PagoEfectivo.java
+++ TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio05/model/PagoEfectivo.java
@@ -1,5 +1,50 @@
 package ar.edu.unju.fi.ejercicio05.model;
 
-public class PagoEfectivo {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import ar.edu.unju.fi.ejercicio05.interfaces.Pago;
+
+public class PagoEfectivo implements Pago{
+	private double montoPagado;
+	private LocalDate fechaPago;
+	
+	public PagoEfectivo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PagoEfectivo(double montoPagado, LocalDate fechaPago) {
+		this.montoPagado = montoPagado;
+		this.fechaPago = fechaPago;
+	}
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	
+	public void realizarPago(double monto) {
+		montoPagado = monto * 0.9;		
+	}
+
+	@Override
+	public void imprimirRecibo() {
+		// TODO Auto-generated method stub
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		System.out.println("\nFecha de pago: " + fechaPago.format(formatter)
+							+ "\nMonto Pagado: " + montoPagado);
+		
+	}
 
 }
--- TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio05/model/PagoTarjeta.java
+++ TP02_PVSilvaJael/src/ar/edu/unju/fi/ejercicio05/model/PagoTarjeta.java
@@ -1,5 +1,56 @@
 package ar.edu.unju.fi.ejercicio05.model;
 
-public class PagoTarjeta {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import ar.edu.unju.fi.ejercicio05.interfaces.Pago;
+
+public class PagoTarjeta implements Pago{
+	private String numTarjeta;
+	private LocalDate fechaPago;
+	private double montoPagado;
+	public PagoTarjeta() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getNumTarjeta() {
+		return numTarjeta;
+	}
+	public void setNumTarjeta(String numTarjeta) {
+		this.numTarjeta = numTarjeta;
+	}
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+	public PagoTarjeta(String numTarjeta, LocalDate fechaPago, double montoPagado) {
+		this.numTarjeta = numTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+	
+	@Override
+	public void realizarPago(double monto) {
+		// TODO Auto-generated method stub
+		montoPagado = monto * 1.5;
+		
+	}
+	@Override
+	public void imprimirRecibo() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		System.out.println("\nNumero de tarjeta: " + numTarjeta + 
+				"\nFecha de pago: " + fechaPago.format(formatter)
+				+ "\nMonto Pagado: " + montoPagado);
+		
+		
+		
+	}
+	
 }
